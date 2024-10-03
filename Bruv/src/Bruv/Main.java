a4555b508cda9d4a87ee8bf274808159d9644854
huzai <huzai@LAPTOP-IS4V3QL2.mshome.net>
2024-10-03 19:17:02 +0530

    Main

---
+++ Bruv/src/Bruv/Main.java
@@ -0,0 +1,7 @@
+package Bruv;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Main Project");
+	}
+}	
--- Bruv/src/module-info.java
+++ Bruv/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module test9 {
+module Bruv {
 }
